232d0e8ebaf7920b24c4de7da7b8ad0434d9c976
Pádua Lima <padualima.dev@gmail.com>
2022-04-27 22:09:58 -0300

    :hammer: changes for record serializer

--- app/serializers/record_serializer.rb
+++ app/serializers/record_serializer.rb
@@ -1,4 +1,28 @@
 class RecordSerializer < ActiveModel::Serializer
-  attributes :id, :kind, :by_survivor
-  belongs_to :survivor
+  attributes :id
+  attribute :record_status do
+    object.kind
+  end
+  attribute :reported_survivor do
+    {
+      id: object.survivor.id,
+      name: object.survivor.name,
+      gender: object.survivor.gender,
+      actual_status: object.survivor.status,
+      location: [
+        object.by_survivor.location
+      ]
+    }
+  end
+  attribute :recorded_by_survivor do
+    {
+      id: object.by_survivor.id,
+      name: object.by_survivor.name,
+      gender: object.by_survivor.gender,
+      actual_status: object.by_survivor.status,
+      location: [
+        object.by_survivor.location
+      ]
+    }
+  end
 end
